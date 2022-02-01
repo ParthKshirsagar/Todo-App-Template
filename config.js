8df7340510bb22f791f2c46c93aec21d5e67098a
ParthKshirsagar <74238278+ParthKshirsagar@users.noreply.github.com>
2022-02-01 16:51:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDw-_n5zHslagcevlRpg_ZlSb8rf8Ro1hM",
+  authDomain: "storytelling-app-6ede1.firebaseapp.com",
+  databaseURL: "https://storytelling-app-6ede1-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-6ede1",
+  storageBucket: "storytelling-app-6ede1.appspot.com",
+  messagingSenderId: "712363145092",
+  appId: "1:712363145092:web:e9f78d58bdbb2920a68fd1"
+};
+
+export default firebaseConfig;
